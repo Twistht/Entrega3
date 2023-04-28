5a5823780a0cf8ae74f8f9c41d7722012366ed14
Emi Lobaccaro <109561429+Twistht@users.noreply.github.com>
2023-04-27 23:48:20 -0300

    Entrega3

---
+++ entega3ARPROGconMetodos/src/main/java/org/example/ResultadoEnum.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ResultadoEnum {
+//    final String ruta = "C:\\Users\\Emi\\Desktop\\Entrega3ArProg\\entega3ARPROGconMetodos\\src\\puntaje.csv";
+//    final String ruta1 = "C:\\Users\\Emi\\Desktop\\Entrega3ArProg\\entega3ARPROGconMetodos\\src\\ResultadosPartidos.txt";
+//    try{
+//        try {
+//            FileReader fileReader = new FileReader(ruta);
+//            FileReader fileReader1 = new FileReader(ruta1);
+//            if ()
+//
+//
+//        } catch (FileNotFoundException ex) {
+//            throw new RuntimeException(ex);
+//        }
+//    }
+}
